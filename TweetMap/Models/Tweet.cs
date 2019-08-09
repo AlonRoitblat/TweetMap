965aa6dadcbc996bca965c34ec05c0d1d086c449
Alon Roitblat <alonroitblat@gmail.com>
2019-08-09 17:48:21 +0300

    Remove unused parts and add tweet model

---
+++ TweetMap/Models/Tweet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetMap.Models
+{
+    public class Tweet
+    {
+        public int tweetID { get; set; }
+        public string tweetText { get; set; }
+        public Point Location { get; set; }
+    }
+}
